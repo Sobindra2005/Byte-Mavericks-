995de829774d433624424d78eab1ef0550287091
SakshamPhuyal <phuyalsaksham1@gmail.com>
2025-09-05 07:41:42 +0545

    chore: remove placeholder content from Home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 
 import './App.css'
+import Home from './Home'
 import MainLayout from './layout'
 
 function App() {
   return (
     <>
+   
+    <Home/>
+
       <MainLayout>
         <Routes>
           {/* <Route path="/" element={isLoggedIn ? <Course /> : <Home />} /> */}
---
+++ src/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        ghsdhs
+      
+    </div>
+  )
+}
+
+export default Home
