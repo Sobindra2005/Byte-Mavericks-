dcad763407a9eea1bd72eb753f72d12990ff3d67
sobindra2005 <sobhindra2005@gmail.com>
2025-09-05 07:34:26 +0545

    feat: initialize project with Vite, React, and Tailwind CSS
    - Added package.json with dependencies for React, Tailwind CSS, and Vite.
    - Included Vite configuration for React and Tailwind CSS.
    - Created main application structure with App component and MainLayout.
    - Added basic routing structure in App component.
    - Implemented layout with header and sidebar navigation.
    - Included SVG assets for Vite and React logos.
    - Set up Tailwind CSS for styling in App.css.

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import MainLayout from './layout'
+
+function App() {
+  return (
+    <>
+      <MainLayout>
+        <Routes>
+          {/* <Route path="/" element={isLoggedIn ? <Course /> : <Home />} /> */}
+        </Routes>
+      </MainLayout>
+    </>
+  )
+}
+
+export default App
