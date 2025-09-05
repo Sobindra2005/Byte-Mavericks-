334a3ec163652ee6d16eb0b1227d256011f98e99
SakshamPhuyal <phuyalsaksham1@gmail.com>
2025-09-05 11:58:46 +0545

    Enhance Course component layout and styling; update MainLayout to include signup path in authentication check; improve Home component text and structure

--- src/component/layout/layout.jsx
+++ src/component/layout/layout.jsx
@@ -5,7 +5,7 @@ import Navbar from "../../nav/Navbar";
 
 const MainLayout = ({ children }) => {
     const location = useLocation();
-    const isNotAuthenticated = (location.pathname === '/' || location.pathname === '/login')
+    const isNotAuthenticated = (location.pathname === '/' || location.pathname === '/login' || location.pathname === '/signup')
     return (
         <div className="max-h-screen h-screen flex flex-col">
             <header className="bg-[#142814] text-white p-2 ">
